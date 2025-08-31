6c66210b65345ff6ab21720efed110f10db6827f
RASIC <rylee.rasic@gmail.com>
2025-08-31 08:58:45 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <section id="conta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
+
 import { Hero } from "@/components/sections/hero";
 import { Services } from "@/components/sections/services";
 import { About } from "@/components/sections/about";
 import { Testimonials } from "@/components/sections/testimonials";
-import { Contact } from "@/components/sections/contact";
 import { TrustIndicators } from "@/components/sections/trust-indicators";
 import { WhyChooseUs } from "@/components/sections/why-choose-us";
 import { Cta } from "@/components/sections/cta";
@@ -16,7 +16,6 @@ export default function Home() {
       <WhyChooseUs />
       <Testimonials />
       <Cta />
-      <Contact />
     </div>
   );
 }
