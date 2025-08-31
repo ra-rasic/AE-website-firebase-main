a72d236538ee438af5e5cab2599ef48843919035
RASIC <rylee.rasic@gmail.com>
2025-08-31 09:28:56 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="flex f

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -117,7 +117,7 @@ export default function AboutPage() {
           </p>
           
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
-            <Button asChild size="lg" className="cta-button text-white">
+            <Button asChild size="lg" className="bg-accent hover:bg-accent/90 text-accent-foreground">
               <Link href="/contact">Get Free Quote</Link>
             </Button>
             <Button asChild size="lg" variant="outline" className="border-white text-white hover:bg-white/10">
