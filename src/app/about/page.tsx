5d82f9ea8210d3b1a82a05aa3ff6415a8c7bae6a
RASIC <rylee.rasic@gmail.com>
2025-08-31 09:26:32 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="contai

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -70,7 +70,7 @@ export default function AboutPage() {
         </div>
       </section>
 
-      <section className="py-16">
+      <section className="py-16 bg-white">
         <div className="container mx-auto px-4">
           <div className="max-w-4xl mx-auto text-center">
             <h2 className="text-3xl font-bold text-gray-900 mb-6">Proudly Serving Southwest Florida</h2>
