e0f56889f201c784832ce6a5556ee50b98b72bd6
RASIC <rylee.rasic@gmail.com>
2025-09-02 23:13:52 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <section class="py

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@
 import { Hero } from "@/components/sections/hero";
 import { Services } from "@/components/sections/services";
 import { About } from "@/components/sections/about";
-import { TrustIndicators } from "@/components/sections/trust-indicators";
 import { Cta } from "@/components/sections/cta";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Hero />
-      <TrustIndicators />
       <Services />
       <About />
       <Cta />
